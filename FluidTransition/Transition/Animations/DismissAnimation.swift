d70107ee18b726e12ab9470d2973087b2dfdb736
Mikhail Rubanov <akaDuality@yandex.ru>
2019-07-14 15:01:56 +0300

    Simplify confirmance to UIViewControllerAnimatedTransitioning protocol

--- FluidTransition/Transition/Animations/DismissAnimation.swift
+++ FluidTransition/Transition/Animations/DismissAnimation.swift
@@ -8,29 +8,36 @@
 
 import UIKit
 
-class DismissAnimation: NSObject, UIViewControllerAnimatedTransitioning {
+class DismissAnimation: NSObject {
     let duration: TimeInterval = 0.3
+    
+    private func animator(using transitionContext: UIViewControllerContextTransitioning) -> UIViewImplicitlyAnimating {
+        let from = transitionContext.view(forKey: .from)!
+        let initialFrame = transitionContext.initialFrame(for: transitionContext.viewController(forKey: .from)!)
+        
+        let animator = UIViewPropertyAnimator(duration: duration, curve: .easeOut) {
+            from.frame = initialFrame.offsetBy(dx: 0, dy: initialFrame.height)
+        }
+        
+        animator.addCompletion { (position) in
+            transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
+        }
+        
+        return animator
+    }
+}
+
+extension DismissAnimation: UIViewControllerAnimatedTransitioning {
     func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
         return duration
     }
     
-    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) { }
-    
-    var animator: UIViewPropertyAnimator?
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        let animator = self.animator(using: transitionContext)
+        animator.startAnimation()
+    }
     
     func interruptibleAnimator(using transitionContext: UIViewControllerContextTransitioning) -> UIViewImplicitlyAnimating {
-        let from = transitionContext.view(forKey: .from)!
-        
-        let initialFrame = transitionContext.initialFrame(for: transitionContext.viewController(forKey: .from)!)
-        
-        animator = UIViewPropertyAnimator(duration: duration, curve: .easeOut) {
-            from.frame = initialFrame.offsetBy(dx: 0, dy: initialFrame.height)
-        }
-        
-        animator!.addCompletion { (position) in
-            transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
-        }
-        
-        return animator!
+        return self.animator(using: transitionContext)
     }
 }
--- FluidTransition/Transition/Animations/PresentAnimation.swift
+++ FluidTransition/Transition/Animations/PresentAnimation.swift
@@ -8,30 +8,37 @@
 
 import UIKit
 
-class PresentAnimation: NSObject, UIViewControllerAnimatedTransitioning {
+class PresentAnimation: NSObject {
     let duration: TimeInterval = 0.3
+
+    private func animator(using transitionContext: UIViewControllerContextTransitioning) -> UIViewImplicitlyAnimating {
+        let to = transitionContext.view(forKey: .to)!
+        let finalFrame = transitionContext.finalFrame(for: transitionContext.viewController(forKey: .to)!)
+        
+        to.frame = finalFrame.offsetBy(dx: 0, dy: finalFrame.height)
+        let animator = UIViewPropertyAnimator(duration: duration, curve: .easeOut) {
+            to.frame = finalFrame
+        }
+        
+        animator.addCompletion { (position) in
+            transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
+        }
+        
+        return animator
+    }
+}
+
+extension PresentAnimation: UIViewControllerAnimatedTransitioning {
     func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
         return duration
     }
     
-    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) { }
-    
-    var animator: UIViewPropertyAnimator?
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        let animator = self.animator(using: transitionContext)
+        animator.startAnimation()
+    }
     
     func interruptibleAnimator(using transitionContext: UIViewControllerContextTransitioning) -> UIViewImplicitlyAnimating {
-        let to = transitionContext.view(forKey: .to)!
-        
-        let finalFrame = transitionContext.finalFrame(for: transitionContext.viewController(forKey: .to)!)
-        
-        to.frame = finalFrame.offsetBy(dx: 0, dy: finalFrame.height)
-        animator = UIViewPropertyAnimator(duration: duration, curve: .easeOut) {
-            to.frame = finalFrame
-        }
-        
-        animator!.addCompletion { (position) in
-            transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
-        }
-        
-        return animator!
+        return self.animator(using: transitionContext)
     }
 }
